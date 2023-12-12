266e4936776f4288cfa96b7b93747c9c30a509f2
Abi <abi.sivasith.world@gmail.com>
2023-12-12 19:46:38 +1300

    created file to rewrite letter frequency in JS

---
+++ letter_frequency.js
@@ -0,0 +1,8 @@
+/*
+Function that take a given encoded test and counts letter frequency. 
+With the knowlege of the most common frequency an affine cipher can be easily decrypted.
+
+Written By: Abilash Sivasith
+*/
+
+
