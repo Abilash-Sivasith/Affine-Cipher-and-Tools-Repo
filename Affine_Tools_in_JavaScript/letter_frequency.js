841c4b2d44ffc20e6e81be844f19360a7a31cbc4
Abi <abi.sivasith.world@gmail.com>
2023-12-13 22:01:43 +1300

    Completted letter frequency function in JS

--- Affine_Tools_in_JavaScript/letter_frequency.js
+++ Affine_Tools_in_JavaScript/letter_frequency.js
@@ -6,3 +6,25 @@ Written By: Abilash Sivasith
 */
 
 
+function findLetterFreqeuncy(text){
+    // finds the frequency of letters
+    let letterCountDict = {};
+
+    for (let i = 0; i < text.length;i++) {
+        let letter = text[i].toLowerCase();
+        if (letter != ' '){
+            if (!(letter in letterCountDict)){
+                letterCountDict[letter] = 1;
+            } else {
+                letterCountDict[letter]++;
+            }
+        }
+    }
+    return letterCountDict
+}
+
+
+let exampleText = 'Hello world'
+let test1 = ''
+test1 = findLetterFreqeuncy(exampleText)
+console.log(test1)
